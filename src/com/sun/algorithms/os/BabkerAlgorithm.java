43e77db0549a89692aee5a3d736015c36008b2d9
mrright2019 <3082671298@qq.com>
2017-12-26 13:44:10 +0800

    os

--- src/com/sun/algorithms/os/BabkerAlgorithm.java
+++ src/com/sun/algorithms/os/BabkerAlgorithm.java
@@ -5,12 +5,14 @@ public class BabkerAlgorithm {
 	private int max[][] = new int[5][3]; // max resource the process need
 	private int current[] = new int[3];// the num of recourse now
 	private int now[][] = new int[5][3]; // the num of five process having
+	private int need[][] = new int[5][3];
 	Scanner scanner = new Scanner(System.in);
 	
 	public BabkerAlgorithm() {
 		System.out.println("please input the num of three rescoures");
 		for(int i=0;i<3;i++) {
 			resource[i] = scanner.nextInt();
+			current[i] = resource[i];
 		}
 		System.out.println("please input the max num of the recourse the five processes need");
 		for(int i=0;i<5;i++) {
@@ -25,6 +27,7 @@ public class BabkerAlgorithm {
 			{
 				now[i][j] = scanner.nextInt();
 				current[j] -= now[i][j];
+				need[i][j] = max[i][j]-now[i][j];
 			}
 		}
 		
@@ -56,11 +59,41 @@ public class BabkerAlgorithm {
 				return false;
 			}
 		}
-		
-		System.out.println("Success");
-		for(int i=0;i<3;i++) {
-			now[n][i] += arr[i];
-			current[i] -= arr[i];
+		int temp[] = new int[3];
+		int safe[] = {0,0,0,0,0};
+		int ec = 0;
+		for(int i=0;i<3;i++){
+//			temp[i] = current[i] -arr[i];
+			temp[i] += current[i]+ now[n][i];
+			
+		}
+		while(ec<5){
+			for(int i=0;i<5;i++){
+				if(safe[i]==1) continue;
+				if(temp[0]>=need[i][0] && temp[1]>=need[i][1] &&temp[2]>=need[i][2] ){
+					safe[i]=1;
+					temp[0] += now[i][0];
+					temp[1] += now[i][1];
+					temp[2] += now[i][2];
+				}
+			}
+			ec++;
+		}
+		if(safe(temp)){
+			System.out.println("Success");
+			for(int i=0;i<3;i++) {
+				now[n][i] +=arr[i];
+				current[i] -= arr[i];
+			}
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	public boolean safe(int arr[]){
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]==0) return false;
 		}
 		return true;
 	}
